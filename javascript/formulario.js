2e8067bf8b82e3208e976c052cd86ff2acc42338
BrianDiaz04 <104173790+BrianDiaz04@users.noreply.github.com>
2024-05-26 02:20:59 -0300

    Add files via upload

--- javascript/formulario.js
+++ javascript/formulario.js
@@ -1,23 +1,25 @@
 console.log ("Holaaa mundo");
-
+//1
 var input_nombre = document.getElementById("nombre");
 var input_edad = document.getElementById("edad");
 var input_email = document.getElementById("email");
 var input_submit = document.getElementById("submit");
+var input_submit1 = document.getElementById("submit1");
+var input_button = document.getElementById("btnShow");
 
 console.log(input_nombre);
 console.log(input_edad);
 console.log(input_email);
 console.log(input_submit);
+console.log(input_submit1);
 
-
-
-
+//2
 input_submit.addEventListener("click", enviarFormulario);
 
 function enviarFormulario(event){
 event.preventDefault();
 
+//3
 var valor_nombre = input_nombre.value;
 var valor_edad = input_edad.value;
 var valor_email = input_email.value;
@@ -41,5 +43,24 @@ elemento_feedback.classList.remove("oculto");
 elemento_formulario = document.getElementById("formulario");
 elemento_formulario.classList.add("oculto");
 
+}
+//segundo boton
+
+input_submit1.addEventListener("click", aparecerFormulario);
+
+function aparecerFormulario(event){
+event.preventDefault();
+
+var placeholder_nombre = document.getElementById("nombre-placeholder");
+var placeholder_email = document.getElementById("email-placeholder");
+
+elemento_feedback = document.getElementById("feedback1");
+elemento_feedback.classList.remove("oculto1");
+
+elemento_feedback = document.getElementById("feedback");
+elemento_feedback.classList.add("oculto");
 
 }
+
+
+
